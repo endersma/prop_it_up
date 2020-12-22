445f78795a280870b0d43a83a62829f42ab248f5
Mark Enders <endersma@gmail.com>
2020-12-21 16:22:49 -0800

    first commit

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component{
+    constructor(props) {
+        super(props); 
+        this.state = {
+            age: this.props.age
+        };
+    }
+   
+
     render(){
         const { firstName, lastName, age, hairColor } = this.props;
         return(
             <div>
                 <h1>{ lastName }, { firstName }</h1>
-                <h3>Age: { age }</h3>
+                <h3>Age: { this.state.age }</h3>
                 <h3>Hair Color: { hairColor }</h3>
+                <button onClick={ this.increaseAge }>Birthday Button for { firstName } { lastName }</button>
             </div>
         );
+    }
+    increaseAge = () => {
+        this.setState( { 'age': this.state.age +1 } );
     }
 }
 
